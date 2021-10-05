8b112a62151bcc5c946444d865e1c10ff8f93b1a
Karol <swidrak.karol@gmail.com>
2021-10-05 23:43:38 +0200

    Changed update start date

--- bet/jobs/updater.py
+++ bet/jobs/updater.py
@@ -4,5 +4,5 @@ from bet.functions import givePoint
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(givePoint, 'interval', days=1, start_date='2021-10-05 10:30:00')
+    scheduler.add_job(givePoint, 'interval', days=1, start_date='2021-10-05 12:00:00')
     scheduler.start()
