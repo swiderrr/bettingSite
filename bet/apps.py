b1dcd73c053d5cae705ba11766e2233f0d571893
Karol <swidrak.karol@gmail.com>
2021-10-06 20:09:20 +0200

    deleted

--- bet/apps.py
+++ bet/apps.py
@@ -5,6 +5,3 @@ class BetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bet'
 
-    def ready(self):
-        from bet.jobs import updater
-        updater.start()
